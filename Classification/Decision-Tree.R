29988de13c5f8fb02958b4c22ddfcee085e6d9e6
Rolando <87973999+Rgzsat@users.noreply.github.com>
2023-07-12 11:06:20 +0300

    Update Decision-Tree.R

--- Classification/Decision-Tree.R
+++ Classification/Decision-Tree.R
@@ -2,7 +2,7 @@
 rm(list=ls())
 
 #loading and plotting data
-load(file="C:\\Users\\47406\\Downloads\\Data Mining course\\practice_02\\kdata.RData")
+load(file="C:\\Users\\47406\\Downloads\\kdata.RData")
 
 #data plotting - visualization
 plot(x[,1],x[,2], col=scales::alpha(x[,3],0.3), pch=20)
